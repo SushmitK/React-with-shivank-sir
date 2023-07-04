a756e4aa39d57e7001986bb6231ea7bf49fe5614
SushmitKatkale <113032361+SushmitK@users.noreply.github.com>
2023-07-04 14:24:22 +0530

    I used class as well as function component for the creating components

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class Car extends React.Component{
+  render(){
+    return <h1>Class Component</h1>
+  }
+}
+
+const Car2 = () => {
+  return (
+    <h1>Function Component</h1>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React With Shivank Sir</h1>
+      <Car />
+      <Car2 />
     </div>
   );
 }
