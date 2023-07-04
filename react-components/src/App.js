c305238d92a195f667f96991d484292b07cabce2
SushmitKatkale <113032361+SushmitK@users.noreply.github.com>
2023-07-04 14:33:06 +0530

    i used props to display diffrent things in components

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 
-class Car extends React.Component{
-  render(){
-    return <h1>Class Component</h1>
-  }
+// class Car extends React.Component{
+//   render(){
+//     return <h1>Class Component</h1>
+//   }
+// }
+
+const Car = (props) => {
+  return (
+    <>
+    <h1>I Am a {props.color} Car.</h1>
+    </>
+  )
 }
 
-const Car2 = () => {
-  return (
-    <h1>Function Component</h1>
-  )
+const Garage = (props) => {
+    return(
+      <>
+        <h1>Who lives in this {props.name} garage</h1>
+        <Car color="Black"/>
+      </>
+    )
 }
 
 function App() {
   return (
     <div className="App">
       <h1>React With Shivank Sir</h1>
-      <Car />
-      <Car2 />
+      <Garage name="Indian"/>
     </div>
   );
 }
