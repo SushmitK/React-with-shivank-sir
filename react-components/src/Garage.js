881fdf63d694294f6d34fed5ec925ff41d2d2804
SushmitKatkale <113032361+SushmitK@users.noreply.github.com>
2023-07-05 14:42:48 +0530

    Learn About List in react

--- react-components/src/Garage.js
+++ react-components/src/Garage.js
@@ -1,9 +1,25 @@
 import Car from './Car';
 
+const car = [
+  {id:1,color: "Red"},
+  {id:1,color: "Black"},
+  {id:1,color: "White"},
+  {id:1,color: "Blue"},
+  {id:1,color: "Dark Blue"},
+  {id:1,color: "Yellow"},
+]
+
 const Garage = (props) => {
     return(
       <>
         <h1>Who lives in this {props.name} garage</h1>
+        <ul>
+          {
+            car.map((car) => {
+              return (<li><Car id={car.id} color={car.color}/></li>)
+            })
+          }
+        </ul>
         <Car color="Black"/>
       </>
     )
